7689dd02935314b484c018f66ee0aecd1832482a
김정완 <krwd1117@gmail.com>
2024-08-13 23:37:16 +0900

    프로젝트 생성

---
+++ Hatssue/HatssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  HatssueApp.swift
+//  Hatssue
+//
+//  Created by Jeongwan Kim on 8/13/24.
+//
+
+import SwiftUI
+
+@main
+struct HatssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
